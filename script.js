70c9a7832d58aab0edf357468059045177955b4e
Gustavo <107372982+GuhhSchool@users.noreply.github.com>
2024-06-09 13:54:55 -0300

    Arquivos Finais

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const resultBoxElement = document.querySelector('.div--result');
 const resultTextElement = document.querySelector('.p--result-text');
 const divInputElement = document.querySelector('.div--input');
 const inputElement = document.querySelector('.input--name');
+const restartButtonElement = document.querySelector('.img--restart');
 
 const contents = [
     {
@@ -15,11 +16,11 @@ const contents = [
         alternatives: [
             {
                 content: 'Hmm... achei interessante.',
-                response: 'Desde o começo, se interessou pela parceria com a IA.',
+                response: 'Desde o começo, G{nome} se interessou pela parceria com a IA.',
             },
             {
                 content: 'Tenho certeza que é golpe.',
-                response: 'No começo, achou que era golpe e ficou com medo de conversar com a IA.',
+                response: 'No começo, G{nome} achou que era golpe e ficou com medo de conversar com a IA.',
             },
         ],
     },
@@ -28,13 +29,13 @@ const contents = [
         history: "No dia seguinte, no colégio, você decide contar para a sua turma sobre o que viu no dia anterior. Então, a professora de matemática resolve passar novas atividades de casa para a turma resolver, com brinde para quem acertar tudo. O que você faz?",
         alternatives: [
             {
-                content: 'Converso com essa pessoa misteriosa, para que me ajude a encontrar informações relevantes para a lição e explique de forma que facilite o meu entendimento.',
+                content: 'Converso com essa pessoa misteriosa para que me ajude a encontrar informações relevantes para a lição e explique de forma que eu entenda facilmente.',
                 response: 'Um tempo depois, quis arriscar e ver o que ela tinha a oferecer, já que precisava de ajuda em suas atividades escolares.',
                 subcategory: 2,
             },
             {
-                content: 'Resolvo a lição com base nas conversas que tive com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.',
-                response: 'Um tempo depois, optou por não usar as novidades dela pois pensava que se sairia bem na atividades escolares que tinha, usando as próprias fontes de conhecimento.',
+                content: 'Resolvo a lição com base em conversas com colegas, pesquisas na internet e conhecimentos próprios sobre o tema.',
+                response: 'Um tempo depois, optou por não usar as novidades dela pois acreditava que se sairia bem na atividades escolares que tinha, usando apenas as próprias fontes de conhecimento.',
                 subcategory: 1,
             },
         ],
@@ -46,21 +47,21 @@ const contents = [
         alternatives: [
             {
                 content: 'Defendo a ideia de que a ajuda dessa pessoa misteriosa pode criar novas oportunidades e até melhorar habilidades humanas.',
-                response: 'Por conta dessa decisão, G{nome} não conseguiu a melhor nota da turma, apesar do grande esforço... Então percebeu que a IA ajudaria em situações como essa, onde daria mais oportunidades e ajuda para que as pessoas possam adquirir mais habilidades humanas.',
+                response: 'Por conta dessa decisão, G{nome} não conseguiu a melhor nota da turma apesar do grande esforço... Então percebeu que a IA ajudaria em situações como essa, onde daria mais oportunidades e ajuda para que as pessoas possam adquirir mais habilidades humanas.',
             },
             {
-                content: 'Me preocupo com as pessoas que perderão seus méritos por conta de pessoas como a que ajudou seu colega, e defendo a importância de valorizar quem trabalha duro.',
-                response: 'Por conta dessa decisão, G{nome} não conseguiu a melhor nota da turma apesar do grande esforço, tirando a conclusão de que inteligências artificiais apenas prejudicaria pessoas que se esforçam e trabalham duro.',
+                content: 'Me preocupo com as pessoas que perderão seus méritos por conta de pessoas como a "misteriosa" e defendo a importância de valorizar quem trabalha duro.',
+                response: 'Por conta dessa decisão, G{nome} não conseguiu a melhor nota da turma apesar do grande esforço, tirando a conclusão de que inteligências artificiais apenas prejudicariam pessoas que se esforçam e trabalham duro.',
             },
         ],
     },
     {
         order: 2,
-        history: "No dia seguinte, ao entregar as lições, seu amigo estudioso errou apenas uma questão. Se aproximando de você, a professora diz que você acertou todas! Então, ela decide fazer perguntas sobre as lições das quais você consegue responder facilmente mesmo você tendo aprendido apenas no dia anterior. O que você pensa?",
+        history: "No dia seguinte, ao entregar as lições, seu amigo estudioso errou apenas uma questão. Se aproximando de você, a professora diz que você acertou todas! Então, ela decide fazer perguntas sobre as lições das quais você consegue responder facilmente mesmo você tendo aprendido rapidamente no dia anterior. O que você pensa?",
         subcategory: 2,
         alternatives: [
             {
-                content: 'Defendo a ideia de que a ajuda dessa pessoa misteriosa pode criar novas oportunidades e até melhorar habilidades humanas',
+                content: 'Defendo a ideia de que a ajuda dessa pessoa misteriosa pode criar novas oportunidades e até melhorar habilidades humanas.',
                 response: 'Com a ajuda da IA, G{nome} conseguiu se destacar e tirou a maior nota da turma! Então concluiu que usar IA para adquirir conhecimento e ter ajuda será uma das formas de melhorar o mundo.',
             },
             {
@@ -75,10 +76,10 @@ const contents = [
         alternatives: [
             {
                 content: 'Começar a usar mais essa ferramenta para auxiliar quando você precisar.',
-                response: 'Um tempo passou e, quando G{nome} descobriu do que a IA realmente é capaz, se surpreendeu tanto que resolveu começar a usá-la frequentemente até os dias atuais, ajudando diariamente em tarefas cotidianas.',
+                response: 'Um tempo passou e, quando G{nome} descobriu do que a IA realmente é capaz, se surpreendeu tanto que resolveu começar a usá-la frequentemente até os dias atuais, onde ela ajuda diariamente em tarefas cotidianas e profissionais.',
             },
             {
-                content: 'Expulsar a IA de casa, pois pode ser muito perigosa se começarem a usar ela com más intenções.',
+                content: 'Expulsar a IA de casa. Pois pode ser muito perigosa se começarem a usar ela com más intenções.',
                 response: 'Um tempo passou e, quando G{nome} descobriu do que a IA realmente é capaz, se assustou tanto que nunca mais quis vê-la em sua frente, preferindo ler livros e adquirir conhecimentos através de estudos.',
             },
         ],
@@ -86,8 +87,8 @@ const contents = [
 ];
 
 let currentIndex = 0;
-let name = '';
-let currentHistory = 'G{nome}, Em 2049... <br>';
+let username = '';
+let currentHistory = 'Biografia de: <span class="color-yellow">G{nome}</span><br>Ano: <span class="color-yellow">2049</span><br>';
 
 
 class showHistory {
@@ -122,7 +123,7 @@ class showHistory {
     #setTitle() {
         textHistoryElement.animate({ color: '#0B0D20'}, { duration: 0, fill: 'forwards' });
         textHistoryElement.textContent = this.#currHistoryObj.history;
-        
+        console.log(this.#indexHistory);
         setTimeout(() => {
             textHistoryElement.animate({ color: '#D7F9FF'}, { duration: 250, fill: 'forwards' });
             textHistoryElement.style.color = '#D7F9FF';
@@ -148,13 +149,13 @@ class showHistory {
     }
 
     #showResult() {
-        textHistoryElement.innerHTML = currentHistory;
+        textHistoryElement.innerHTML = currentHistory.replaceAll('G{nome}', username);
         beforeHeight();
         buttonBoxElement.textContent = "";
+        restartButtonElement.style.display = "flex";
+        document.documentElement.style.setProperty('--color-border', '#ffef08');
     }
 }
-
-// new showHistory(currentIndex);
 
 function beforeHeight() {
     mainBoxAnimElement.style.height = mainBoxElement.clientHeight + 'px';
@@ -171,14 +172,18 @@ inputElement.addEventListener("keypress", function(event) {
     if (event.key !== 'Enter') return;
 
     verifyName();
-    });
+});
 
 function verifyName() {
     const temporaryName = inputElement.value;
     
     var letters = /^[A-Za-z çÇáÁíÍãÃóÓéÉêÊâÂúÚüÜ]+$/;
-    if (temporaryName.length <= 2) return alert('Ops! Seu nome precisa ter pelo menos 3 caracteres.');
-    if (!temporaryName.match(letters)) return alert('Ops! Seu nome não pode conter caracteres especiais (exceto espaços)');
+    if (temporaryName.length) {
+        if (temporaryName.length <= 2) return alert('Ops! Seu nome precisa ter pelo menos 3 caracteres.');
+        if (!temporaryName.match(letters)) return alert('Ops! Seu nome não pode conter caracteres especiais (exceto espaços)');
+        username = temporaryName.trim().split(/ +/g).map(m => m.replace(m[0], m[0].toUpperCase())).join(' ');
+    }
+    else username = 'S/n';
 
     preview();
 }
@@ -187,9 +192,10 @@ function preview() {
     divInputElement.style.opacity = 0;
     textHistoryElement.style.opacity = 0;
 
-
     setTimeout(() => {
+        divInputElement.style.display = 'none';
         textHistoryElement.textContent = 'Vamos começar';
+        beforeHeight();
         textHistoryElement.style.opacity = 1;
     }, 1000);
 
